75ccafc4496c2be60bdb64f47d20d06c02f1cbe0
Sagar Singh <sagar.01ko@gmail.com>
2023-10-21 10:46:41 +0530

    Updated bg

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import {Outlet} from 'react-router-dom'
 
 function App() {
   return (
-    <div className='p-[1px] bg-[url(./imgAssests/home/home-bg.jpg)] bg-cover h-[100vh] text-white'>
+    <div className='p-[1px] bg-[url(/imgAssests/home/home-bg.jpg)] bg-cover h-[100vh] text-white'>
       <Navbar/>
       <Outlet/>
       </div>
